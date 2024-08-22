317959f364da5d0bcf47bf2f3a17288ca316a358
Domingos Canhanga <canhanga96@gmail.com>
2024-08-22 07:44:24 +0100

    rotas e method http

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import http from 'node:http';
 
 const server = http.createServer((req, res) => {
+  const { url, method } = req;
 
+  if (method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuários')
+  }
+  if(method ==='POST' && url === '/users') {
+    return res.end('Criando um novo usuário')
+  }
  return res.end('Hello world!!!')
 })
 
