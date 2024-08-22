9b5aaead78af01a742754e8dfe80cca5961a28c4
Domingos Canhanga <canhanga96@gmail.com>
2024-08-22 05:05:38 +0100

    node watch

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import http from 'node:http';
 
 const server = http.createServer((req, res) => {
 
- return res.end('Hello world!')
+ return res.end('Hello world!!!')
 })
 
-server.listen(3333)
+server.listen(3333,()=> {console.log('listening on port 3333')})
