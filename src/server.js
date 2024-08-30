a55b6eceb2a723451dd53c2acbd4aa04c79bef7b
Domingos Canhanga <canhanga96@gmail.com>
2024-08-30 09:39:14 +0100

    json middleware

--- src/server.js
+++ src/server.js
@@ -1,28 +1,14 @@
 import http from 'node:http';
+import { json } from './middlewares/json.js';
 const users = []
 const server = http.createServer(async (req, res) => {
-  // Parse do body com
-
-  const buffers = []
-  for await (const chunk of req) {
-    console.log({chunk})
-    buffers.push(chunk)
-
-  }
-  try {
-req.boy = JSON.parse(Buffer.concat(buffers).toString())
-
-  } catch {
-   req.body = null
-  }
-
-
+ await json(req, res)
 
   const { url, method } = req;
 
   if (method === 'GET' && url === '/users') {
     console.log(users)
-     return res.setHeader('Content-type', 'application/json').end(JSON.stringify(users))
+     return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
